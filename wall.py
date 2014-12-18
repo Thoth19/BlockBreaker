a462a459965ed45c0397263e164adfb601a4aafc
Thoth19 <kpseipp@gmail.com>
2014-12-18 11:34:44 -0800

    increase size of block and wall

--- wall.py
+++ wall.py
@@ -3,7 +3,7 @@ class WallSprite(pygame.sprite.Sprite):
     ''' This class is a 16 by 16 blue block to serve as the border of game'''
     def __init__(self, position):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([16,16])
+        self.image = pygame.Surface([64,64])
         self.image.fill((0,0,255))
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
@@ -14,7 +14,7 @@ class BlockSprite(pygame.sprite.Sprite):
 		''' position is a 2-tuple, color isa 3-tuple in RGB
 		points is the value of destroying the block, integer. ''' 
 		pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([16,16])
+        self.image = pygame.Surface([64,64])
         self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
