d736fecff7d425f315cc14c19a91fd60d51a9b57
Thoth19 <kpseipp@gmail.com>
2014-12-18 11:29:07 -0800

    first commit

---
+++ paddle.py
@@ -0,0 +1,24 @@
+import math, os, pygame
+X_MAX = 1024
+class PaddleSprite(pygame.sprite.Sprite):
+    '''
+    This is the user controlled paddle in a game of Pong
+    '''
+    def __init__(self, position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([128,16])
+        self.rect = self.image.get_rect()
+        self.rect.x=position[0]
+        self.rect.y=position[1]
+    def move(self, position):
+        ''' moves to given position'''
+        if position[0] + 128 > X_MAX:
+            # The paddle itself is 128 pixels long
+            self.rect.x = X_MAX -128
+        else:
+            self.rect.x = position[0]
+    def update(self):
+        '''
+        Empty function so that all sprite groups can be updated
+        '''
+        pass
