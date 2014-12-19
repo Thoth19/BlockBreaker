0f304fa046449e3e28dfb7344d19d67b52cdac6e
Thoth19 <kpseipp@gmail.com>
2014-12-18 18:26:39 -0800

    game is playable. addedscore, lives, mulitple levels

--- block_breaker.py
+++ block_breaker.py
@@ -2,16 +2,20 @@ import math, os, pygame, random
 from wall import*
 from paddle import*
 from ball import*
+score = 0 
+lives  = 2
+level = 25
 
-lives  = 3
-level = 1
 # plan is to allow three instances of balls going off of the board. if a board is cleared 
 # the player will get an extra life, and be moved to a new board. That can take place via command lines
 # in order to give the player a break 
+
+pygame.init()
 while lives > 0:
     #Init
+    level += 1
+    lives += 1
 
-    pygame.init()
     screen = pygame.display.set_mode([1024,768])
     clock = pygame.time.Clock()
 
@@ -22,7 +26,7 @@ while lives > 0:
     non_ball_sprites_group = pygame.sprite.Group()
     all_sprites_group = pygame.sprite.Group()
 
-    p1 = PaddleSprite((512,768-64-32))
+    p1 = PaddleSprite((512,768-64-32),(True, False))
     paddle_group.add(p1)
     all_sprites_group.add(p1)
     non_ball_sprites_group.add(p1)
@@ -31,17 +35,20 @@ while lives > 0:
     all_sprites_group.add(ball)
     for i in range(16):
         for j in range(12):
-            if i == 0 or i == 15 or j == 0 or j == 11:
-                wall = WallSprite((i*64,j*64))
-                wall_group.add(wall)
-                all_sprites_group.add(wall)
-                non_ball_sprites_group.add(wall)
+            if i == 0 or i == 15: 
+                wall = WallSprite((i*64,j*64),(False,True))
+            if j == 0 or j == 11:
+                wall = WallSprite((i*64,j*64),(True, False))
+            wall_group.add(wall)
+            all_sprites_group.add(wall)
+            non_ball_sprites_group.add(wall)
+
         #creates walls around the game board
     #each level gets more lucrative, and the colors should get more interesting
     for i in range(12):
-        block1 = BlockSprite((i*64+128,2*64),(00,255,00),level * 3)
-        block2= BlockSprite((i*64+128,3*64),(100,255,0),level * 2)
-        block3 = BlockSprite((i*64+128,4*64),(0,255,100),level * 1)
+        block1 = BlockSprite((i*64+128,2*64),(00,max(0,255-10*level),00),level * 3, (False, False))
+        block2 = BlockSprite((i*64+128,3*64),(100,max(0,255-10*level),0),level * 2, (False, False))
+        block3 = BlockSprite((i*64+128,4*64),(0,max(0,255-10*level),100),level * 1, (False, False))
         block_group.add(block1)
         block_group.add(block2)
         block_group.add(block3)
@@ -55,6 +62,7 @@ while lives > 0:
     while not(done):
         clock.tick(30)
         screen.fill((255,0,0))
+        all_sprites_group.update()
 
         for event in pygame.event.get():
             if event.type != pygame.MOUSEBUTTONDOWN:
@@ -71,6 +79,7 @@ while lives > 0:
             ball = BallSprite((400,320),(random.randint(-3,3),random.randint(1,3)))
             ball_group.add(ball)
             all_sprites_group.add(ball)
+            print "lives: ",lives
         for b in ball_group:
             if b.rect.y > p1.rect.y:
                 #you can't catch it so delete it
@@ -85,16 +94,42 @@ while lives > 0:
                 for p in non_ball_sprites_group:
                     if b.velocity[0] != 0:
                         if new_rect_x.colliderect(p.rect):
-                            b.x_dir()
+                            if not(p.orient[0]):
+                                b.x_dir()
+                            if p in block_group:
+                                b.velocity = b.velocity[0],b.velocity[1]+random.randint(-1,1)
+                                score += p.value * level
+                                all_sprites_group.remove(p)
+                                non_ball_sprites_group.remove(p)
+                                block_group.remove(p)
+                            b.velocity = b.velocity[0]+1,b.velocity[1]
+                            
                     if b.velocity[1] != 0:
                         if new_rect_y.colliderect(p.rect):
-                            b.y_dir()
-            print b.rect.x, b.rect.y
+                            if not(p.orient[1]):
+                                b.y_dir()
+                            if p in block_group:
+                                b.velocity = b.velocity[0]+random.randint(-1,1),b.velocity[1]
+                                score += p.value * level
+                                all_sprites_group.remove(p)
+                                non_ball_sprites_group.remove(p)
+                                block_group.remove(p)
+                            b.velocity = b.velocity[0],b.velocity[1]+1
+                            
+                if b.velocity ==(0,0):
+                    b.velocity = 0,1
+            #print b.rect.x, b.rect.y
 
+        if len(block_group) == 0:
+            done = True
+            score += 10*level
 
-        all_sprites_group.update()
         all_sprites_group.draw(screen)
         pygame.display.flip()
     pygame.display.quit()
     print "Good Job! You beat stage %d." %level
-    x = raw_input("Enter anything to continue")
+    print "Your score is %d" %score
+    print "You still have %d lives left. Plues you get a free one for beating the level" %lives
+    x = raw_input("Enter anything to continue: ")
+print "Well you finally lost with a final score of %d" %score
+print "And you made it to level %d" %level
